3f99478832b0fc3b087619596a92fabb376d3a35
luca@igor <luca@igor>
2024-09-22 17:59:42 +0200

    updated producer / consumer example

--- examples/example_producer_consumer.py
+++ examples/example_producer_consumer.py
@@ -4,13 +4,13 @@ import asyncio
 logger = get_logger()
 
 from eric_sse.entities import Message, MessageQueueListener, MESSAGE_TYPE_CLOSED
-from eric_sse.prefabs import SSEChannel, ThreadPoolListener
+from eric_sse.prefabs import ThreadPoolListener, DataProcessingChannel
 
 
 class Producer:
 
     @staticmethod
-    def produce_num(c: SSEChannel, l: MessageQueueListener, num: int):
+    def produce_num(c: DataProcessingChannel, l: MessageQueueListener, num: int):
         for i in range(1, num):
             c.dispatch(l.id, Message(type='counter', payload=i))
         c.dispatch(l.id, Message(type=MESSAGE_TYPE_CLOSED))
@@ -27,13 +27,11 @@ class Consumer(ThreadPoolListener):
 async def main():
 
     # Here you can control message deliver frequency
-    channel = SSEChannel(stream_delay_seconds=0)
+    channel = DataProcessingChannel(stream_delay_seconds=0)
     # And max wprkers num
     consumer = Consumer(max_workers=10)
     channel.register_listener(consumer)
-    Producer.produce_num(c=channel, l=consumer, num=10000)
-
-    # In this scenario, dispathing order should be not respected
+    Producer.produce_num(c=channel, l=consumer, num=20)
 
     await consumer.start()
     async for _ in await channel.message_stream(consumer):
