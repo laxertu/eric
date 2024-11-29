5d69a36808748bd20e9c2c49de448d9089b1ce08
luca@igor <luca@igor>
2024-11-29 18:18:26 +0100

    0.6.0

--- examples/distributed_app.py
+++ examples/distributed_app.py
@@ -24,15 +24,20 @@ async def main():
     alice = create_listener(ssc)
     bob = create_listener(ssc)
 
+    # Bob says hello to Alice
     ssc.dispatch(alice.id, Message(type='hello', payload={'sender_id': bob.id, 'payload': 'hello!'}))
+
+    # Alice will receive Bob's message then a 'stop' one
     ssc.dispatch(alice.id, Message(type='stop'))
 
-    await bob.start()
     await alice.start()
+    await bob.start()
 
     print("Alice's queue")
     async for m in await ssc.message_stream(alice):
         print(f'output {m}')
+
+    # Let's enqueue a stop to Bob
     ssc.dispatch(bob.id, Message(type='stop'))
 
 
