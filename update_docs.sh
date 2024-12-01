e0cf3aeb00f5591f6fe33b421a9bf2ff28f4abd6
luca@igor <luca@igor>
2024-12-01 17:08:00 +0100

    minor changes

--- update_docs.sh
+++ update_docs.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 cd docs
 rm -rf build/html/
+rm -rf build/markdown/
+
 make markdown
 make html
-#cp build/markdown/index.md ../README.md
-cp build/markdown/docs.md ../docs.md
+
 rm -rf ../docs_html/
+rm -rf ../docs_markdown/
+
 cp -rf build/html/ ../docs_html/
+cp -rf build/html/ ../docs_markdown/
