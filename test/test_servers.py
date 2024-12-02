370151c9ec5b1228a4e3c8a4e725ace2d0868ee4
luca@igor <luca@igor>
2024-12-02 18:47:01 +0100

    tests refactor

--- test/test_servers.py
+++ test/test_servers.py
@@ -1,9 +1,11 @@
-import json
+import json, asyncio
 from unittest import IsolatedAsyncioTestCase
 from unittest.mock import MagicMock
 
+from eric_sse.clients import SocketClient
 from eric_sse.prefabs import SSEChannel
 from eric_sse.servers import SocketServer, SSEChannelContainer
+SOCKET_FILE_DESCRIPTOR_PATH = 'socketserver_e2e_test.sock'
 
 
 class SocketServerTestCase(IsolatedAsyncioTestCase):
@@ -14,7 +16,36 @@ class SocketServerTestCase(IsolatedAsyncioTestCase):
         channel_container_mock.get = MagicMock(return_value=channel_mock)
         SocketServer.cc = channel_container_mock
 
-        self.sut = SocketServer('')
+        self.sut = SocketServer(SOCKET_FILE_DESCRIPTOR_PATH)
+        self.channel_mock = channel_mock
+
+    async def test_server(self):
+
+        client = SocketClient(SOCKET_FILE_DESCRIPTOR_PATH)
+        server = SocketServer(SOCKET_FILE_DESCRIPTOR_PATH)
+        asyncio.get_running_loop().create_task(server.main())
+        await asyncio.sleep(0)
+
+
+        server_response = await client.send_payload({
+            'v': 'b',
+            'c': '1',
+            't': 'txt',
+            'p': 'Hi there!'
+        })
+
+        self.assertEqual(server_response, SocketServer.ACK)
+        await server.shutdown()
+
+class SocketServerIntgrationTestCase(IsolatedAsyncioTestCase):
+
+    def setUp(self):
+        channel_container_mock = MagicMock(SSEChannelContainer)
+        channel_mock = MagicMock(SSEChannel)
+        channel_container_mock.get = MagicMock(return_value=channel_mock)
+        SocketServer.cc = channel_container_mock
+
+        self.sut = SocketServer(SOCKET_FILE_DESCRIPTOR_PATH)
         self.channel_mock = channel_mock
 
     async def test_send(self):
@@ -29,3 +60,4 @@ class SocketServerTestCase(IsolatedAsyncioTestCase):
             ...
 
         self.channel_mock.broadcast.assert_called_once()
+
