efafff5a5408b9e6f448cb9f38762fdabbdd4b94
luca <laxertu@gmail.com>
2025-02-02 23:01:23 +0100

    new update_docs.sh

--- update_docs.sh
+++ update_docs.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 cd docs
-rm -rf build/html/
-rm -rf build/markdown/
-rm -rf build/doctrees/
 
-make markdown
-make html
+echo "Generating documentation..."
+make markdown -q
+make html -q
 
-rm -rf ../docs_html/
-rm -rf ../docs_markdown/
-
+echo "Copying files..."
 cp -rf build/html/ ../docs_html/
 cp -rf build/markdown/ ../docs_markdown/
 
 # patch
 cp -rf source/_static ../docs_markdown/_static
+echo "Done"
