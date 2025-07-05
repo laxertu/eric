ea2e200c69830e8f64066bb4723c7144b45e38e6
luca <laxertu@gmail.com>
2025-07-05 19:30:02 +0200

    minor changes

--- eric_sse/queue.py
+++ eric_sse/queue.py
@@ -45,11 +45,3 @@ class InMemoryQueue(Queue):
         self.__messages = []
 
 
-class RepositoryError(Exception):
-    """
-    Raised when an unexpected error occurs while trying to fetch messages from a queue.
-
-    Concrete implementations of :class:`Queue` should wrap here the unexpected exceptions they catch before raising, and
-    an :class:`eric_sse.exception.NoMessagesException` when a pop is requested on an empty queue.
-    """
-    ...
--- eric_sse/repository.py
+++ eric_sse/repository.py
@@ -51,3 +51,12 @@ class InMemoryMessageQueueRepository(AbstractMessageQueueRepository):
     def delete(self, listener_id: str):
         pass
 
+
+class RepositoryError(Exception):
+    """
+    Raised when an unexpected error occurs while trying to fetch messages from a queue.
+
+    Concrete implementations of :class:`Queue` should wrap here the unexpected exceptions they catch before raising, and
+    an :class:`eric_sse.exception.NoMessagesException` when a pop is requested on an empty queue.
+    """
+    ...
