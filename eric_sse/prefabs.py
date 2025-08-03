f8209872ee6f52c56a7b13009ac2e1acbc531964
luca <laxertu@gmail.com>
2025-08-03 23:44:37 +0200

    * deleted SSChannel.payload_adapter * added connections repo class name to SSChannel value in persistence implementation

--- eric_sse/prefabs.py
+++ eric_sse/prefabs.py
@@ -47,7 +47,7 @@ class SSEChannel(AbstractChannel, PersistableChannel):
         self.stream_delay_seconds = setup['stream_delay_seconds']
         self.retry_timeout_milliseconds = setup['retry_timeout_milliseconds']
 
-        connections = self.__connections_repository.load_all()
+        connections = self._connections_repository.load_all()
         for connection in connections:
             self.register_connection(listener=connection.listener, queue=connection.queue)
 
