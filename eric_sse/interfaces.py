60e2ef810cf79ef5cce7fc3b6aa6f794e5a8a2ed
luca <laxertu@gmail.com>
2025-08-21 20:31:39 +0200

    rc1

--- eric_sse/interfaces.py
+++ eric_sse/interfaces.py
@@ -3,6 +3,9 @@ from typing import Iterable
 
 from eric_sse.entities import AbstractChannel
 from eric_sse.connection import Connection
+from eric_sse.listener import MessageQueueListener
+from eric_sse.queues import AbstractQueue
+# TODO add repos dependencies to connection one, improve builder
 
 class ConnectionRepositoryInterface(ABC):
 
@@ -39,4 +42,31 @@ class ChannelRepositoryInterface(ABC):
 
     @abstractmethod
     def delete(self, channel_id: str):
+        pass
+
+class QueueRepository(ABC):
+
+    @abstractmethod
+    def load(self, connection_id: str) -> AbstractQueue:
+        pass
+
+    @abstractmethod
+    def persist(self, queue: AbstractQueue):
+        pass
+
+    @abstractmethod
+    def delete(self, connection_id: str):
+        pass
+
+class ListenerRepository(ABC):
+    @abstractmethod
+    def load(self, connection_id: str) -> MessageQueueListener:
+        pass
+
+    @abstractmethod
+    def persist(self, listener: MessageQueueListener):
+        pass
+
+    @abstractmethod
+    def delete(self, connection_id: str):
         pass
